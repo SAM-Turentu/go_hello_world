39f4963928a8c392879fc5daa4e761f64ad61b1d
SAM-MBP <ainiruoxia@outlook.com>
2023-02-06 21:52:31 +0800

    fix:go一般结构

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// 常量定义
+const PI = math.Pi
+
+// 全局变量的声明和赋值
+var name = "gopher"
+
+// 一般类型声明
+type newType int
+
+// 结构的声明
+type gopher struct{}
+
+// 接口的声明
+type golang interface{}
 
 func main() {
 	fmt.Println("hello wold")
 	fmt.Println("golang")
 	fmt.Println("good good study, day day up")
+	fmt.Printf("math pi = %f", PI)
+	fmt.Println()
 
 }
