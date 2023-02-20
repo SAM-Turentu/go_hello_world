a50f4c606edc179e6284df6d3c28952f7f59954e
SAM-Turentu <SAM-Turentu@outlook.com>
2023-02-20 17:27:28 +0800

    feat: 切片copy，双链表

--- SecondLearn.go
+++ SecondLearn.go
@@ -70,18 +70,58 @@ func Second() {
 	//fmt.Println(a) // [0,1,2,3]
 	//a = append([]int{-3,-2,-1}, a...) // 在开头添加1个切片
 	//fmt.Println(a) // [-3 -2 -1 0 1 2 3]
-	// todo 切片链式操作
+
+	////切片中添加一个切片
+	//单链式操作
 	//b := make([]int, 10)
-	//fmt.Println(b)  // [0,0,0,0,0,0,0,0,0,0]
-	//b = append(b[:2], append([]int{10}, b[2:]...)...) // 在第2个位置插入10
-	//fmt.Println(b)  // [0,0,10,0,0,  0,0,0,0,0]
-	//b = append(b[:3], append([]int{1,2,3}, b[3:]...)...) // 在第3个位置插入切片
-	//fmt.Println(b)  // [0,0,10,1,2,3,0,0,0,  0,0,0,0,0]
+	//fmt.Println(b)                                         // [0,0,0,0,0,0,0,0,0,0]
+	//b = append(b[:2], append([]int{10}, b[2:]...)...)      // 在第2个位置插入10
+	//fmt.Println(b)                                         // [0,0,10,0,0,  0,0,0,0,0]
+	//b = append(b[:3], append([]int{1, 2, 3}, b[3:]...)...) // 在第3个位置插入切片
+	//fmt.Println(b)                                         // [0,0,10,1,2,3,0,0,0,  0,0,0,0,0]
 	//
 	//b = make([]int, 10)
-	//fmt.Println(b)  // [0,0,0,0,0,  0,0,0,0,0]
+	//fmt.Println(b) // [0,0,0,0,0,  0,0,0,0,0]
+	//a := append([]int{5}, b[7:]...)
+	//fmt.Printf("a: %v\n", a)
 	//b = append(b[:4], append([]int{5}, b[7:]...)...) // 在第3个位置插入切片
-	//fmt.Println(b) // [0 0 0 0 5 0 0 0]
+	//fmt.Println(b)                                   // [0 0 0 0 5 0 0 0]
+	//
+	////使用append也可以删除索引元素
+	//b = append(b[:4], b[5:]...) // 删除 b[4] = 5 的元素
+	//fmt.Printf("删除b[4]后，b:%v\n", b)
+
+	//endregion
+
+	//region 切片copy
+	// 按照其中较小的那个数组切片的元素个数进行复制
+	a := make([]int, 5, 10)
+	fmt.Printf("a: %v, len(a): %d, cap(a): %d\n", a, len(a), cap(a))
+	a[0] = 1 // 向a切片中添加5个元素
+	a[1] = 2 // 向a切片中添加5个元素
+	a[2] = 3 // 向a切片中添加5个元素
+	a[3] = 4 // 向a切片中添加5个元素
+	a[4] = 5 // 向a切片中添加5个元素
+	fmt.Printf("a: %v\n", a)
+	//b := []int{}
+	b := make([]int, 3, 10)
+
+	fmt.Printf("b: %v, len(b): %d, cap(b): %d\n", b, len(b), cap(b))
+	copy(b, a) // 将a复制给b，len(b) = 3 所以只复制a[:3]
+	fmt.Printf("b: %v, len(b): %d, cap(b): %d\n", b, len(b), cap(b))
+
+	c := make([]int, 10, 10)
+	fmt.Printf("c: %v, len(c): %d, cap(c): %d\n", c, len(c), cap(c))
+	copy(c, a) // 将c复制给a，len(a) = 5 所以只复制c[:5]
+	fmt.Printf("c: %v, len(c): %d, cap(c): %d\n", c, len(c), cap(c))
+	fmt.Printf("a: %v, len(a): %d, cap(a): %d\n", a, len(a), cap(a))
+
+	//endregion
+
+	//region 双链表
+	new_list := New() // 新建一个链表
+	//new_list.insertValue()
+
 	//endregion
 
 	//region
@@ -95,7 +135,7 @@ func Second() {
 	fmt.Println("************************Second END************************")
 }
 
-// 可以写一个in方法
+// region 可以写一个in方法
 func in(p int, arr []int) bool {
 	result := false
 	for _, v := range arr {
@@ -109,3 +149,104 @@ func in(p int, arr []int) bool {
 //func in(s string, arr []int) bool {
 //
 //}
+
+//endregion
+
+// region 定义链表  https://zhuanlan.zhihu.com/p/404754444
+// Element 定义链表中的节点结构体
+type Element struct {
+	next, prev *Element
+	list       *List // 元素所在的链表
+	Value      interface{}
+}
+
+// List 表示双向链表
+type List struct {
+	/*
+		根节点，链表的起点，不存值
+		直接访问 root.prev root.next 分别是链表的前一个几点 和 后一个节点
+	*/
+	root Element
+	len  int // 链表的长度，不包括根节点
+}
+
+// Init 链表初始化（清空链表）
+func (l *List) Init() *List {
+	l.root.next = &l.root
+	l.root.prev = &l.root
+	l.len = 0
+	return l
+}
+
+// New 创建一个新链表
+func New() *List {
+	return new(List).Init()
+}
+
+// Len 查看链表长度
+func (l *List) Len() int {
+	return l.len
+}
+
+// Front 返回链表首节点
+func (l *List) Front() *Element {
+	if l.len == 0 {
+		return nil
+	}
+	return l.root.next
+}
+
+// Back 返回链表的末节点
+func (l *List) Back() *Element {
+	if l.len == 0 {
+		return nil
+	}
+	return l.root.prev
+}
+
+// insert 在链表特定元素后插入新元素
+func (l *List) insert(e, at *Element) *Element {
+	e.prev = at      // 将e的前节点指向at
+	e.next = at.next // 将原有的at后一个节点 给 e的后一个节点
+	e.prev.next = e  // e的前一个节点的后一个节点 是 e本身
+	e.next.prev = e  // e的后一个节点的前一个节点 是 e本身
+	e.list = l       // l 元素所在的链表
+	l.len++
+	return e
+}
+
+// 直接向链表插入一个值
+func (l *List) insertValue(v interface{}, at *Element) *Element {
+	return l.insert(&Element{Value: v}, at)
+}
+
+// 删除链表指定节点
+func (l *List) remove(e *Element) *Element {
+	e.prev.next = e.next
+	e.next.prev = e.prev
+	e.next = nil // 赋空，避免内存泄露
+	e.prev = nil
+	e.list = nil
+	l.len--
+	return e
+}
+
+// 将一个节点e移动到另一个节点后
+func (l *List) move(e, at *Element) *Element {
+	if e == at {
+		return e
+	}
+	//先删除一个元素
+	e.prev.next = e.next
+	e.next.prev = e.prev
+
+	//将元素插入在一个元素后
+	e.prev = at
+	e.next = at.next
+	e.prev.next = e
+	e.next.prev = e
+	//链表长度不变
+	return e
+}
+
+//endregion
