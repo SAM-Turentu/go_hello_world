c853a20c923b06cc1541849c5a259bde98558535
SAM-Turentu <SAM-Turentu@outlook.com>
2023-03-02 11:10:49 +0800

    feat: range;并发中使用的map;nil可以被赋值;switch

--- SecondLearn.go
+++ SecondLearn.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"sync"
 )
 
 func Second() {
-	fmt.Println("************************Second Learn Golang************************")
+	fmt.Println("*********************Second Learn Golang*********************")
 	fmt.Println("Second Learn Golang")
 
 	//region 浮点数简写和科学计数法
@@ -24,17 +24,17 @@ func Second() {
 	//endregion
 
 	//region if 中 一个值在数组中的判断
-	arr := []int{1, 2, 3, 4}
-	//if 1 in arr{} // 在python等语言中可以使用 in，go中不能使用
-	for _, v := range arr {
-		if v == 1 {
-
-		}
-	}
-	_boo, _ := strconv.ParseBool("0")
-	if _boo {
-		fmt.Println(_boo)
-	}
+	//arr := []int{1, 2, 3, 4}
+	////if 1 in arr{} // 在python等语言中可以使用 in，go中不能使用
+	//for _, v := range arr {
+	//	if v == 1 {
+	//
+	//	}
+	//}
+	//_boo, _ := strconv.ParseBool("0")
+	//if _boo {
+	//	fmt.Println(_boo)
+	//}
 
 	//endregion
 
@@ -119,10 +119,53 @@ func Second() {
 	//endregion
 
 	//region 双链表
-	a := []int{1, 2, 3, 4, 5}
-	fmt.Printf("a : %v\n", a)
-	//e := Element{}
-	//e.Value = a
+	//l := list.New() // 初始化一个新链表
+	//fmt.Printf("双向链表的长度：%d\n", l.Len())
+	//l.PushFront(10) // 向链表中添加元素
+	//l.PushFront(11)
+	//l.PushFront("推一个元素")
+	//l.PushFront(Students{name: "", age: 18, sex: 1, phone: "1829200"})
+	//fmt.Printf("查看链表最后一个值：%v\n", l.Front().Value) // 查看链表最后一个值
+	//fmt.Printf("查看链表第一个值：%v\n", l.Back().Value)   //查看链表第一个值
+	//
+	///*
+	//	遍历双向链表的所有元素
+	//	先进先出，第一个被打印的是最后一个添加到链表中的元素
+	//*/
+	//fmt.Println("************************开始遍历链表数据************************")
+	//for e := l.Front(); e != nil; e = e.Next() {
+	//	fmt.Println(e.Value)
+	//}
+
+	//endregion
+
+	//region range 返回的是引用
+	//arr := []int{10,20, 30,40}
+	//var a *int
+	//for i, v := range arr {
+	//	fmt.Printf("value: %d, value-attr: %X elem-addr %X\n", v, &v, &arr[i])
+	//	a = &v
+	//}
+	//fmt.Printf("&a: %v, *a: %v\n", a, *a)
+	//endregion
+
+	//region sync.Map 并发环境中使用map
+	//sync_map()
+	//endregion
+
+	//region nil 赋值【不建议使用】
+	//fmt.Println(nil)
+	//fmt.Printf("nil 类型 %T\n", nil)  // nil 没有类型
+	//nil := 1  // nil 不是关键词或保留字，可以当做变量使用【不建议使用】
+	//fmt.Println(nil)
+	//endregion
+
+	//region switch
+	//var a = "daddy" // 或 num
+	//switch a {
+	//case "mum", "daddy":
+	//	fmt.Println("family")
+	//}
 	//endregion
 
 	//region
@@ -133,7 +176,7 @@ func Second() {
 
 	//endregion
 
-	fmt.Println("************************Second END************************")
+	fmt.Println("*************************Second END*************************")
 }
 
 // region 可以写一个in方法
@@ -153,181 +196,206 @@ func in(p int, arr []int) bool {
 
 //endregion
 
-// region 定义链表  https://zhuanlan.zhihu.com/p/404754444
+// region 定义链表  https://zhuanlan.zhihu.com/p/404754444   ==  import container/list
 // Element 定义链表中的节点结构体
-type Element struct {
-	next, prev *Element
-	list       *List // 元素所在的链表
-	Value      interface{}
-}
-
-// List 表示双向链表
-type List struct {
-	/*
-		根节点，链表的起点，不存值
-		直接访问 root.prev root.next 分别是链表的前一个几点 和 后一个节点
-	*/
-	root Element
-	len  int // 链表的长度，不包括根节点
-}
-
-// Init 链表初始化（清空链表）
-func (l *List) Init() *List {
-	l.root.next = &l.root
-	l.root.prev = &l.root
-	l.len = 0
-	return l
-}
-
-// New 创建一个新链表
-func New() *List {
-	return new(List).Init()
-}
-
-// Len 查看链表长度
-func (l *List) Len() int {
-	return l.len
-}
-
-// Front 返回链表首节点
-func (l *List) Front() *Element {
-	if l.len == 0 {
-		return nil
-	}
-	return l.root.next
-}
-
-// Back 返回链表的末节点
-func (l *List) Back() *Element {
-	if l.len == 0 {
-		return nil
-	}
-	return l.root.prev
-}
-
-// insert 在链表特定元素后插入新元素
-func (l *List) insert(e, at *Element) *Element {
-	e.prev = at      // 将e的前节点指向at
-	e.next = at.next // 将原有的at后一个节点 给 e的后一个节点
-	e.prev.next = e  // e的前一个节点的后一个节点 是 e本身
-	e.next.prev = e  // e的后一个节点的前一个节点 是 e本身
-	e.list = l       // l 元素所在的链表
-	l.len++
-	return e
-}
-
-// 直接向链表插入一个值
-func (l *List) insertValue(v interface{}, at *Element) *Element {
-	return l.insert(&Element{Value: v}, at)
-}
-
-// 删除链表指定节点
-func (l *List) remove(e *Element) *Element {
-	e.prev.next = e.next
-	e.next.prev = e.prev
-	e.next = nil // 赋空，避免内存泄露
-	e.prev = nil
-	e.list = nil
-	l.len--
-	return e
-}
-
-// 将一个节点e移动到另一个节点后
-func (l *List) move(e, at *Element) *Element {
-	if e == at {
-		return e
-	}
-	//先删除一个元素
-	e.prev.next = e.next
-	e.next.prev = e.prev
-
-	//将元素插入在一个元素后
-	e.prev = at
-	e.next = at.next
-	e.prev.next = e
-	e.next.prev = e
-	//链表长度不变
-	return e
-}
-
-// 删除节点并返回元素值
-func (l *List) Remove(e *Element) interface{} {
-	if e.list == l {
-		//判断节点是否在唉该链表中
-		// 如果链表是空的话，而且e是nil的话会panic
-		l.remove(e)
-	}
-	return e.Value
-}
-
-// 插入一个值到链表头
-func (l *List) PushFront(v interface{}) *Element {
-	return l.insertValue(v, &l.root)
-}
-
-// 在链尾添加一个值
-func (l *List) PushBack(v interface{}) *Element {
-	return l.insertValue(v, l.root.prev)
-}
-
-// 插入一个值到某个节点前面
-func (l *List) InsertBefore(v interface{}, mark *Element) *Element {
-	if mark.list != l {
-		return nil
-	}
-	return l.insertValue(v, mark.prev)
-}
-
-// 插入一个元素在某个节点后
-func (l *List) InsertBack(v interface{}, mark *Element) *Element {
-	if mark.list != l {
-		return nil
-	}
-	return l.insertValue(v, mark)
-}
-
-// 将一个节点移动到链表首
-func (l *List) MoveToFront(e *Element) {
-	if e.list != l || l.root.next != e {
-		return
-	}
-	l.move(e, &l.root)
-}
-
-// 将一个元素移动到链表尾部
-func (l *List) MoveToBack(e *Element) {
-	if e.list != l || l.root.prev != e {
-		return
-	}
-	l.move(e, l.root.prev)
-}
-
-// 将一个节点移动到另一个节点前
-func (l *List) MoveBefore(e, mark *Element) {
-	if e.list != l || e == mark || mark.list != l {
-		return
-	}
-	l.move(e, mark.prev)
-}
-
-// 将一个节点移动到另一个节点后
-func (l *List) MoveAfter(e, mark *Element) {
-	if e.list != l || e == mark || mark.list != l {
-		return
-	}
-	l.move(e, mark)
-}
-
-////在链表后面添加一个链表
-//func (l *List) PushBackList(other *List) {
-//	for i, e := other.Len(), other.Front(); i >0; i, e = i-1, e.Next(){
-//		l.insertValue(e.Value, l.root.prev)
+//type Element struct {
+//	next, prev *Element
+//	list       *List // 元素所在的链表
+//	Value      interface{}
+//}
+//
+//// List 表示双向链表
+//type List struct {
+//	/*
+//		根节点，链表的起点，不存值
+//		直接访问 root.prev root.next 分别是链表的前一个几点 和 后一个节点
+//	*/
+//	root Element
+//	len  int // 链表的长度，不包括根节点
+//}
+//
+//// Init 链表初始化（清空链表）
+//func (l *List) Init() *List {
+//	l.root.next = &l.root
+//	l.root.prev = &l.root
+//	l.len = 0
+//	return l
+//}
+//
+//// New 创建一个新链表
+//func New() *List {
+//	return new(List).Init()
+//}
+//
+//// Len 查看链表长度
+//func (l *List) Len() int {
+//	return l.len
+//}
+//
+//// Front 返回链表首节点
+//func (l *List) Front() *Element {
+//	if l.len == 0 {
+//		return nil
 //	}
+//	return l.root.next
 //}
 //
-////将一个链表逆序添加到链表到前面
-//func (l *List) PushFrontList(other *List) {
-//
+//// Back 返回链表的末节点
+//func (l *List) Back() *Element {
+//	if l.len == 0 {
+//		return nil
+//	}
+//	return l.root.prev
 //}
+//
+//// insert 在链表特定元素后插入新元素
+//func (l *List) insert(e, at *Element) *Element {
+//	e.prev = at      // 将e的前节点指向at
+//	e.next = at.next // 将原有的at后一个节点 给 e的后一个节点
+//	e.prev.next = e  // e的前一个节点的后一个节点 是 e本身
+//	e.next.prev = e  // e的后一个节点的前一个节点 是 e本身
+//	e.list = l       // l 元素所在的链表
+//	l.len++
+//	return e
+//}
+//
+//// 直接向链表插入一个值
+//func (l *List) insertValue(v interface{}, at *Element) *Element {
+//	return l.insert(&Element{Value: v}, at)
+//}
+//
+//// 删除链表指定节点
+//func (l *List) remove(e *Element) *Element {
+//	e.prev.next = e.next
+//	e.next.prev = e.prev
+//	e.next = nil // 赋空，避免内存泄露
+//	e.prev = nil
+//	e.list = nil
+//	l.len--
+//	return e
+//}
+//
+//// 将一个节点e移动到另一个节点后
+//func (l *List) move(e, at *Element) *Element {
+//	if e == at {
+//		return e
+//	}
+//	//先删除一个元素
+//	e.prev.next = e.next
+//	e.next.prev = e.prev
+//
+//	//将元素插入在一个元素后
+//	e.prev = at
+//	e.next = at.next
+//	e.prev.next = e
+//	e.next.prev = e
+//	//链表长度不变
+//	return e
+//}
+//
+//// 删除节点并返回元素值
+//func (l *List) Remove(e *Element) interface{} {
+//	if e.list == l {
+//		//判断节点是否在唉该链表中
+//		// 如果链表是空的话，而且e是nil的话会panic
+//		l.remove(e)
+//	}
+//	return e.Value
+//}
+//
+//// 插入一个值到链表头
+//func (l *List) PushFront(v interface{}) *Element {
+//	return l.insertValue(v, &l.root)
+//}
+//
+//// 在链尾添加一个值
+//func (l *List) PushBack(v interface{}) *Element {
+//	return l.insertValue(v, l.root.prev)
+//}
+//
+//// 插入一个值到某个节点前面
+//func (l *List) InsertBefore(v interface{}, mark *Element) *Element {
+//	if mark.list != l {
+//		return nil
+//	}
+//	return l.insertValue(v, mark.prev)
+//}
+//
+//// 插入一个元素在某个节点后
+//func (l *List) InsertBack(v interface{}, mark *Element) *Element {
+//	if mark.list != l {
+//		return nil
+//	}
+//	return l.insertValue(v, mark)
+//}
+//
+//// 将一个节点移动到链表首
+//func (l *List) MoveToFront(e *Element) {
+//	if e.list != l || l.root.next != e {
+//		return
+//	}
+//	l.move(e, &l.root)
+//}
+//
+//// 将一个元素移动到链表尾部
+//func (l *List) MoveToBack(e *Element) {
+//	if e.list != l || l.root.prev != e {
+//		return
+//	}
+//	l.move(e, l.root.prev)
+//}
+//
+//// 将一个节点移动到另一个节点前
+//func (l *List) MoveBefore(e, mark *Element) {
+//	if e.list != l || e == mark || mark.list != l {
+//		return
+//	}
+//	l.move(e, mark.prev)
+//}
+//
+//// 将一个节点移动到另一个节点后
+//func (l *List) MoveAfter(e, mark *Element) {
+//	if e.list != l || e == mark || mark.list != l {
+//		return
+//	}
+//	l.move(e, mark)
+//}
+//
+//////在链表后面添加一个链表
+////func (l *List) PushBackList(other *List) {
+////	for i, e := other.Len(), other.Front(); i >0; i, e = i-1, e.Next(){
+////		l.insertValue(e.Value, l.root.prev)
+////	}
+////}
+////
+//////将一个链表逆序添加到链表到前面
+////func (l *List) PushFrontList(other *List) {
+////
+////}
+
+//endregion
+
+// region sync.Map 并发环境中使用map
+// 需要并发读写时，要加锁，
+func sync_map() {
+	var s sync.Map
+	//将键值保存到sync.Map
+	s.Store("name", "sam")
+	s.Store("london", []int{10, 20, 30})
+	s.Store("egypt", 200)
+
+	fmt.Println(s.Load("name"))   // 返回 sam true
+	fmt.Println(s.Load("green"))  // 没有键，返回 nil false
+	fmt.Println(s.Load("london")) // 返回 [10 20 30] true
+
+	s.Delete("egypt") // 删除键值对
+
+	//遍历所有sync.Map中的键值对
+	s.Range(func(k, v interface{}) bool {
+		fmt.Println("iterate:", k, v)
+		return true
+	})
+
+}
 
 //endregion
