d46c09762b58e293d063b3737bca5d49426a1af2
SAM-MBP <ainiruoxia@outlook.com>
2023-03-25 09:12:10 +0800

    feat: channel实现生产者与消费者，和channel的其他功能

--- Advancd.go
+++ Advancd.go
@@ -38,11 +38,66 @@ func AdvanceLearn() {
 	//endregion
 
 	//region 时间和日期
-	usetime()
+	//usetime()
 	//endregion
 
-	//region
+	//region 生产者与消费者 并发
 
+	//c := make(chan int)  // 创建一个channel
+	//
+	//go consumer(c) // 并发执行消费者
+	//
+	//// 生产者
+	//for i := 1; i < 10; i++ {  // 从1开始
+	//	c <- i  // 将数据通过channel 传给consumer
+	//}
+	//c <- 0 // 通知并发读consumer结束死循环
+	//<-c  // 等待consumer结束
+
+	//endregion
+
+	//region 单向通道
+	//ch := make(chan int)
+	//var chSendOnly chan<- int = ch // 只写channel
+	//var chRecyOnly <-chan int = ch // 只读channel
+	//// 只写只读都指向相同都地址
+	//fmt.Println(chSendOnly)
+	//fmt.Println(chRecyOnly)
+	//
+	//onlyRead := make(<-chan int)  // 只读channel
+	//onlyWrite := make(chan<- int) // 只写channel
+	//fmt.Println(onlyRead)
+	//fmt.Println(onlyWrite)
+	//
+	//close(onlyWrite) // 关闭channel
+	//endregion
+
+	//region 玩个球 无缓冲channel
+	//ch := make(chan int)
+	//wg.Add(2)
+	//go player("球手1", ch)
+	//go player("球手2", ch)
+	//
+	//ch <- 1
+	//wg.Wait() // 等待游戏结束
+
+	//endregion
+
+	//region 接力跑 无缓冲channel
+	//baton := make(chan int)
+	//wg.Add(1)
+	//go run(baton)
+	//baton <- 1
+	//wg.Wait()
+	//endregion
+
+	//region 超时channel  select case
+	//delayChan()
+	//endregion
+
+	//region 设置cpu核心数
+	//fmt.Println(runtime.NumCPU()) // 处理器核数
+	//runtime.GOMAXPROCS(8)  // 设置需要用到的CPU核心数
 	//endregion
 
 	//region
@@ -51,6 +106,8 @@ func AdvanceLearn() {
 
 	fmt.Println("*************************Advance END*************************")
 }
+
+var wg sync.WaitGroup
 
 // region web 服务
 func Start() {
@@ -166,6 +223,109 @@ func usetime() {
 	fmt.Println(current_time)
 
 }
+
+//endregion
+
+//region 生产者与消费者
+
+// Consumer 消费者
+func consumer(c chan int) {
+	// 开始死循环
+	for {
+		data := <-c    // 将channel数据给data
+		if data == 0 { // channel 数据为0 时，跳出死循环
+			break
+		}
+		fmt.Println(data) // 打印消费读数据
+	}
+	c <- 0 // 通知生产者已经结束循环
+}
+
+//endregion
+
+// region 玩个球
+func player(name string, ch chan int) {
+	defer wg.Done()
+	for {
+		ball, ok := <-ch
+		if !ok {
+			fmt.Printf("Player %s Won.\n", name)
+			return
+		}
+		n := rand.Intn(100)
+		if n%13 == 0 { // 随机数，判断是否丢球，关闭channel
+			fmt.Printf("Player %s Missed.\n", name)
+			close(ch)
+			return
+		}
+		fmt.Printf("Player %s Hit %d\n", name, ball)
+		ball++
+		ch <- ball
+	}
+}
+
+//endregion
+
+// region 跑个步
+func run(baton chan int) {
+	//defer wg.Done()
+	var newRunner int
+	runner := <-baton // 等待接力棒
+	fmt.Printf("Runner %d Running With Baton\n", runner)
+
+	// 创建下一个跑者
+	if runner != 4 {
+		newRunner = runner + 1
+		fmt.Printf("Runner %d To The Line\n", newRunner)
+		go run(baton)
+	}
+
+	time.Sleep(100 * time.Millisecond) // 正在跑步中
+
+	// 最后一个人了，比赛结束
+	if runner == 4 {
+		fmt.Printf("Runner %d Finished, Race Over\n", runner)
+		wg.Done()
+		return
+	}
+	fmt.Printf("Runner %d Exchange With Runner %d\n", runner, newRunner)
+
+	baton <- newRunner
+}
+
+//endregion
+
+// region 超时channel
+func delayChan() {
+	ch := make(chan int)
+	quit := make(chan bool)
+	go func() {
+		for {
+			select {
+			case num := <-ch: // 每个case语句，必须是一个IO操作
+				fmt.Println("num = ", num)
+			case <-time.After(3 * time.Second):
+				fmt.Println("超时")
+				quit <- true
+			}
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		ch <- i
+		time.Sleep(time.Second)
+	}
+	//_, ok := <- quit
+	//if !ok{
+	//	fmt.Println("结束2")
+	//}
+	<-quit
+	fmt.Println("结束")
+}
+
+//endregion
+
+//region
 
 //endregion
 
